59bcce72f9c80b0323d3ce1d08c23872db5853f0
Dhruv Gangal <121995270+DHRUVGANGAL@users.noreply.github.com>
2025-08-10 22:20:16 +0530

    add overflow-x-hidden to Gallery and Tieup components for improved layout

--- app/_components/Gallery.jsx
+++ app/_components/Gallery.jsx
@@ -38,7 +38,7 @@ const Gallery = () => {
   ];
 
   return (
-    <div className="relative  w-full h-auto py-20">
+    <div className="relative  w-full h-auto py-20 overflow-x-hidden">
       <Carousel slides={slideData} autoplay={true} autoplayInterval={3000}  />
     </div>
   );
